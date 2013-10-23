b29b79090a80190514d736fb5f2deb2885924a94
davidebbo <david.ebbo@microsoft.com>
2013-10-22 21:50:22 -0700

    Initial commit

---
+++ SimpleBatchTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Configuration;
+using System.IO;
+using SimpleBatch;
+
+namespace SimpleBatchTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string storageKey = ConfigurationManager.AppSettings["StorageKey"];
+
+            var host = new Host(storageKey, storageKey);
+            host.RunAndBlock();
+        }
+
+        public static void MakeUpperCase(
+            [BlobInput(@"inputfiles\{name}.txt")] TextReader reader,
+            [BlobOutput(@"outputfiles\{name}.txt")] TextWriter writer)
+        {
+            writer.WriteLine(reader.ReadToEnd().ToUpper());
+        }
+    }
+}
